54571beb4f27ca546ab273432561fbcab77edb0a
Firstcome <72813679+EnterStake@users.noreply.github.com>
2023-09-17 23:35:59 +0300

    Create Auto_Unjail.sh

---
+++ Mainnets/Paloma/Auto_Unjail.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+PASWD='your_password'
+DELAY=100 #Delay time in seconds
+ACC_NAME=your key
+NODE=http://localhost:26657
+CHAIN=messenger
+PROJECT=palomad
+TOKEN_NAME=ugrain
+for (( ;; )); do
+        JAIL=$(${PROJECT} q staking validator $( echo "${PASWD}" | ${PROJECT} keys show ${ACC_NAME} --bech val -a) | grep jailed:);        
+        if [[ ${JAIL} == *"false"* ]]; then
+            echo -e "${GREEN}${JAIL} \n"
+        else
+            echo -e "${GREEN}${JAIL} \n"
+            echo -e $( echo "${PASWD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN} --from ${ACC_NAME} --broadcast-mode block  -y) \n;
+            sleep 1
+        fi
+        for (( timer=${DELAY}; timer>0; timer-- ))
+        do
+                printf "* sleep for ${RED}%02d${NC} sec\r" $timer
+                sleep 1
+        done
+done
